8c5bf5be3d696379e0695a639c3b72fc0ddb1a02
GuilinSir <>
2023-06-12 11:30:01 +0800

    Modify: 添加Common项目相关公共组件

---
+++ lib_base/src/main/java/com/guilin/base/BaseApplication.kt
@@ -0,0 +1,13 @@
+package com.guilin.base
+
+import android.app.Application
+
+/**
+ * @description:
+ * @author:  guilin
+ * @email:   308139995@qq.com
+ * @date :   2023/6/12 11:19 AM
+ */
+open class BaseApplication:Application() {
+
+}
---
+++ lib_common/src/main/java/com/guilin/common/CommonApplication.kt
@@ -0,0 +1,12 @@
+package com.guilin.common
+
+import com.guilin.base.BaseApplication
+
+/**
+ * @description:
+ * @author:  guilin
+ * @email:   308139995@qq.com
+ * @date :   2023/6/12 11:16 AM
+ */
+open class CommonApplication : BaseApplication() {
+}
