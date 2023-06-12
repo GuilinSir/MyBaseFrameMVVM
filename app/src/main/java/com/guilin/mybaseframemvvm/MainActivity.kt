9a66b42d1f6b3ae9dc9e976f614eb8c07909599a
GuilinSir <>
2023-06-12 08:56:18 +0800

    Modify: 添加buildSrc模块,对项目参数及依赖版本统一配置

--- app/src/main/java/com/guilin/mybaseframemvvm/MainActivity.kt
+++ app/src/main/java/com/guilin/mybaseframemvvm/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myapplication
+package com.guilin.mybaseframemvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.guilin.mybaseframemvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -0,0 +1,15 @@
+package com.guilin.mybaseframemvvm.build
+
+/**
+ * 项目相关参数配置
+ */
+object BuildConfig {
+    const val compileSdkVersion = 33
+    const val buildToolsVersion = "33.0.1"
+    const val namespace =  "com.guilin.mybaseframemvvm"
+    const val applicationId = "com.guilin.mybaseframemvvm"
+    const val minSdkVersion = 24
+    const val targetSdkVersion = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
