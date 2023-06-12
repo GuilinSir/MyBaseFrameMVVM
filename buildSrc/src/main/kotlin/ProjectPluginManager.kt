88573f1997add6adfd6858fc6b56653bb9879b6b
GuilinSir <>
2023-06-12 09:53:44 +0800

    Modify: 引入依赖库并统一配置管理

---
+++ buildSrc/src/main/kotlin/ProjectPluginManager.kt
@@ -0,0 +1,8 @@
+package com.guilin.mybaseframemvvm.build
+
+object ProjectPluginManager {
+
+    const val AndroidToolsPlugin = "com.android.tools.build:gradle:4.0.1"
+    const val KotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0"
+
+}
