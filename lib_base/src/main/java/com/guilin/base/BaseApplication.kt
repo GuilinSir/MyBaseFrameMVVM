f71cc0f9f25b66892c5df4a309daae8d5637b830
GuilinSir <308139995@qq.com>
2023-06-13 13:49:23 +0800

    Modify: 屏幕适配

--- lib_base/src/main/java/com/guilin/base/BaseApplication.kt
+++ lib_base/src/main/java/com/guilin/base/BaseApplication.kt
@@ -3,6 +3,7 @@ package com.guilin.base
 import android.app.Application
 import com.alibaba.android.arouter.launcher.ARouter
 import com.guilin.base.utils.SpUtils
+import me.jessyan.autosize.AutoSizeConfig
 
 /**
  * @description:
@@ -10,7 +11,7 @@ import com.guilin.base.utils.SpUtils
  * @email:   308139995@qq.com
  * @date :   2023/6/12 11:19 AM
  */
-open class BaseApplication:Application() {
+open class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initialize()
@@ -21,11 +22,13 @@ open class BaseApplication:Application() {
         SpUtils.initMMKV(this)
 
         // 阿里路由 ARouter 初始化
-        if(BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             ARouter.openLog()     // 打印日志
             ARouter.openDebug()   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
         ARouter.init(this)
+        // 屏幕适配 AndroidAutoSize 以横屏高度为基准进行适配
+        //AutoSizeConfig.getInstance().isBaseOnWidth = false
 
     }
 
